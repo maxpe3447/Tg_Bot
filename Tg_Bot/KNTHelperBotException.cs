6159e2171a9ed648fbb233695840fbcaff3a97d5
Asya <82118916+a-sya@users.noreply.github.com>
2021-07-30 14:10:34 +0300

    update

--- Tg_Bot/KNTHelperBotException.cs
+++ Tg_Bot/KNTHelperBotException.cs
@@ -17,7 +17,7 @@ namespace Tg_Bot
         {
             this.whatToDo = whatToDo;
         }
-        public string GetWhatToDo() => whatToDo;//обращение к понлю "что же мне блин делать, а то я никак не пойму что от меня хотят"
 
+        public string GetWhatToDo() => whatToDo;//обращение к понлю "что же мне блин делать, а то я никак не пойму что от меня хотят"
     }
 }
